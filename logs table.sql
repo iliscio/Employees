7770cee1ccdb5d64f5184949f74227abbf7ed52e
Igor Liscio <igor.liscio1@gmail.com>
2022-02-22 17:35:29 -0300

    Modifying script for creation of logs table

--- logs table.sql
+++ logs table.sql
@@ -1,1 +1,1 @@
-Create Table logs(log_id Number, log_message Varchar2(4000), log_dbmessage Varchar2(4000),log_Date Date);
+Create Table logs(log_id Number, process_id number, log_message Varchar2(4000), log_dbmessage Varchar2(4000),log_Date Date);
