c0077a645fc331816eacaf2ce1aefef0cc4f0c11
Igor Liscio <igor.liscio1@gmail.com>
2022-02-23 13:04:36 -0300

    Updating process table script

--- process table.sql
+++ process table.sql
@@ -1,1 +1,1 @@
-Create Table process (Process_id Number, Process_description Varchar2(4000), Start_date Date);
+Create Table process (Previous_Process_id Number, Process_id Number, Process_description Varchar2(4000), Start_date Date);
